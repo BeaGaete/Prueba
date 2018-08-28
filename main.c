e9514c17ed0734690ec2edda9c67c9a17735f4d0
BeaGaete <bgaete@uc.cl>
2018-08-27 23:30:06 -0300

    main T1EDD

--- main.c
+++ main.c
@@ -1,208 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "stack.h"
-#include "stack.c"
-#include "string.h"
+#include "trie.h"
 
-typedef struct Point {
-  struct stack* prt;
-}Point;
+// Esta linea permite usar MAX_LENGTH como una constante en el programa
+// Uso 101 para considerar el caracter que indica el fin de un string
+#define MAX_LENGTH 101
 
-int pop(Stack* stack);
-// void pop_recursive(Stack* stack, int k, FILE *output_file);
-//int main(int argc, char *argv[])
-int main()
-
+typedef struct TrieNode
 {
+	char c;
+	struct TrieNode *parent;
+	struct TrieNode **children;
+	bool is_word;
+	int freq;
+} TrieNode;
 
-	/*if (argc != 2)
+
+// is_word nos marca el fin de la frase o palabra
+// el trienode recibe el caracter que va a guardar y el nodo padre
+// solicitamos memoria para el nodo y su hijo
+struct TrieNode *create_trienode(char c, struct TrieNode *parent)
+{
+	struct TrieNode *node = malloc(sizeof(struct TrieNode));
+	node->c = c;
+	node->parent = parent;
+	node->children = malloc(26*sizeof(struct TrieNode));
+	node->is_word=false;
+	node->freq=NULL;
+	return node;
+}
+
+//destroy_trienode(struct TrieNode *node);
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
 	{
-		printf("Modo de uso: ./solver test.txt\n");
+		printf("Modo de uso: ./solver [database.txt] [queries.txt] [output.txt]\n");
 		return 0;
-	}*/
+	}
+	FILE *database = fopen(argv[1], "r");
+	FILE *queries  = fopen(argv[2], "r");
+	FILE *output   = fopen(argv[3], "w");
 
-	/* Abrimos el archivo input en modo de lectura */
-	//FILE *input_file = fopen(argv[1], "r");
-  FILE *input_file = fopen("../../tests/test_02.txt", "r");
-
-	/* Abrimos el archivo output en modo de escritura */
-	FILE *output_file = fopen("output.txt", "w");
-
-	/* Revisa que el archivo fue abierto correctamente */
-	if (!input_file)
+	if (!database || !queries || !output)
 	{
-		printf("¡El archivo %s no existe!\n", "../tests/test_00.txt");
+		printf("¡Error abriendo los archivos!");
 		return 2;
 	}
 
-	/* Definimos y asignamos las constantes del problema */
-	int n; int m; int l; int i; int k; int t; int z; int bola_sale;
-	fscanf(input_file, "%d %d %d", &n, &m, &l);
 
-  // creo la matriz de nxm y le asigno memoria
-  /*Point *(points[n][m]);
-  for (int t = 0; t < n; t++) {
-    for (int z = 0; z < m; z++){
-      points[t][z] = (Point *)malloc(sizeof(Point));
-      points[t][z] -> prt = stack_init();
-    }
-  }*/
 
-  // confeccionamos la matriz nxm, solictamos memoria e iniciamos los stacks
-  Point*** points = malloc(sizeof(Point**)*n);
-  for (int t = 0; t < n; t++){
-    points[t] = malloc(sizeof(Point*)*m);
-    for (int z = 0; z < m; z++){
-      points[t][z] = malloc(sizeof(Point));
-      points[t][z] -> prt = stack_init();
-    }
-  }
 
-	for (i = 0; i < l; i++) // recorremos las lineas del texto
+
+	//// Ejemplo de lectura de strings:
+
+	// Leo el numero de entradas en la base de datos
+	int n;
+	fscanf(database, "%d", &n);
+
+	// http://kposkaitis.net/blog/2013/03/09/prefix-tree-implementation-in-c/
+	//struct TrieNode *create_tree(){
+	// creamos el trie
+	struct TrieNode *root = create_trienode(' ', NULL);
+	struct TrieNode *ptr = root;
+	int character;
+	int converted;
+	int buffer;
+
+	// Para cada entrada:
+	for (int i = 0; i < n; i++)
 	{
-		/* Definimos las variables del problema */
-		int o; int r; int c; int k;
+		// Obtengo la frecuencia y el largo
+		int freq, length;
+		// Ojo que incluyo un espacio en el formato para que no lo considere como parte del string
+		fscanf(database, "%d %d ", &freq, &length);
+		printf("freq = %d, length = %d\n", freq, length);
 
-		/* Leemos cada linea del archivo */
-		fscanf(input_file, "%d %d %d %d", &o, &r, &c, &k);
-    printf("%d %d %d %d\n", o, r, c, k);
+		// Leo el string aprovechando que se el largo maximo
+		char chars[MAX_LENGTH];
+		fgets(chars, MAX_LENGTH, database); // lee el caracter hasta que encuentra un salto de linea
+		printf("%s\n", chars);
+		for (int j = 0; j <  length; j++) {
+			//printf("%c ", chars[j]);
+			character = chars[j];
 
-		//////////////// Aqui agrega tu código ///////////////////
+			// si el caracter es una letra o espacio lo va a guardar
+			if(isalpha(character) || character == ' ')
+			{
+					converted = character - 97;
+					if(ptr->children[converted] == NULL)
+					{
+							ptr->children[converted] = create_trienode(character, ptr);
+					}
+					// ahora el puntero va a estar en el hijo
+					ptr = ptr->children[converted];
+					printf("este es el valor guardado: %c\n", ptr -> c);
+			}
 
+			if(ptr != root && (!isalpha(character) || buffer == "\n"))
+			{
+					ptr->is_word = true;
+					ptr = root;
+			}
 
-    if(o == 0){
-      push(points[r][c] -> prt, k);
-      printf("Primera bola en posición %d %d de color %d\n", r, c, k);
-      printf("Ultima bola en posición %d %d de color %d\n", r, c, k);
-      printf("Numero de bolas del stack %d\n", points[r][c] -> prt -> count);
-      printf("------------------------------\n");
-    }else{
-      if (points[r][c] -> prt -> count == 0){
-        //fprintf(output_file, "%s en posicion %d %d\n", "vacio", r, c);
-        fprintf(output_file, "%s\n", "vacio");
-      }else{
-        // hay una o más bolas en el satck
-        bola_sale = pop(points[r][c] -> prt); // sscamos una
-        //fprintf(output_file, "%d en posicion %d %d\n", bola_sale, r, c);
-        fprintf(output_file, "%d\n", bola_sale);
-        if (points[r][c] -> prt -> count == 0){
-          if(bola_sale != k){
-            fprintf(output_file, "%s\n", "vacio");
-          }
-        }else{
-          if (bola_sale != k){
-            while(points[r][c] -> prt -> count >= 1){
-              bola_sale = pop(points[r][c] -> prt);
-              fprintf(output_file, "%d\n", bola_sale);
-              if(bola_sale == k){
-                break;
-              }
-            }
-            if(bola_sale != k){
-              fprintf(output_file, "%s\n", "vacio");
-            }
-          }
-        }
-      }
-    }
-      printf("Se sacó bola %d de la posición %d %d\n", bola_sale, r, c);
-      printf("Numero de bolas del stack %d\n", points[r][c] -> prt -> count);
-  }
+			character = buffer;
+			buffer = chars[j + 1];
 
-  // liberamos la memoria de los stack
-  for (t = 0; t < n; t++){
-    for (z = 0; z < m; z++){
-      destroy(points[t][z] -> prt);
-      free(points[t][z]);
-    }
-  }
+		}
+		// cuando se acabe la frase guardamos su frecuencia
+		ptr->freq = freq;
+		printf("la frecuencia de la palabra es: %d\n", ptr -> freq);
+		printf("\n");
+	}
 
-
-  // liberamos la memoria de la matriz
-  for (int t = 0; t < n; t++){
-    free(points[t]);
-  }
-  free(points);
-
-	/* Cerramos los archivos correctamente */
-	fclose(input_file);
-	fclose(output_file);
-
-
-	///////////////// Recuerda que antes de acabar tu programa debes liberar toda la memoria reservada ///////////////////
-
-
-	/* Esta linea indica que el programa termino sin errores */
+	fclose(database);
+	fclose(queries);
+	fclose(output);
 	return 0;
 }
 
-
-void destroy(Stack *stack){
-  free(stack);
-}
-/*void destroy(Stack *stack)
+void destroy_trienode(struct TrieNode *node)
 {
-  int i = 0;
-  Node *tmp = node_init(7);
-  while(i < stack -> count - 1 ){
-    tmp = stack -> first;
-    tmp -> next = stack -> first -> next;
-    free(stack -> first);
-    tmp = tmp -> next;
-    i++;
-  }
-  //free(stack -> last);
-  free(tmp);
-  free(stack);
-}*/
+  int i;
 
-int pop(Stack* stack){
-  int value;
-  Node * temp = node_init(7);
-  temp = stack -> first;
-  Node * previousToTail = node_init(7);
-  // si el stack esta vacío
-  if (stack -> last == NULL){
-    free(temp);
-    return 7;
+  if(node==NULL)
+  {
+      return;
   }
-  else {
-    if (temp == stack -> last) {
-          value = temp -> color;
-          free(stack -> last);
-          //free(temp);
-    } else {
-          previousToTail = temp;
-          while (previousToTail -> next != stack -> last){
-               previousToTail = previousToTail -> next;
-          }
-          value = previousToTail -> next -> color;
-          stack -> last = previousToTail;
-          free(stack -> last -> next);
-          //free(previousToTail);
-    }
-    stack -> count--;
-    //free(temp);
-    return value;
+
+  for(i=0; i<26; i++)
+  {
+      destroy_trienode(node->children[i]);
   }
+
+  free(node->children);
+  free(node);
+  return;
 }
-
-/*void pop_recursive(Stack * stack, int k, FILE *output_file){
-  int bola_sale;
-  bola_sale = pop(stack);
-  if (stack -> count == 0){
-    if (bola_sale == k){
-      fprintf(output_file, "%d\n", k);
-    }else{
-      fprintf(output_file, "%s\n", "vacio");
-    }
-  }
-  else{
-    if (bola_sale == k){
-      fprintf(output_file, "%d\n", k);
-    }else{
-      pop_recursive(stack, k, output_file);
-    }
-  }
-}*/
