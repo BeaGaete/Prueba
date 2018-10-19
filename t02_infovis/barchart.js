9fff2f519019ad639d7f5a6cb4ed52a6bba03da1
BeaGaete <bgaete@uc.cl>
2018-10-19 15:06:32 -0300

    luminosity forks & return first position

--- t02_infovis/barchart.js
+++ t02_infovis/barchart.js
@@ -1,10 +1,10 @@
-const margin = {top: 30, right: 20, bottom: 0, left: 20};
+const margin = {top: 30, right: 20, bottom: 0, left: 20}; // 30 20 0 20
 
-const WIDTH = 500;
-const HEIGHT = 500;
+const WIDTH = 350;
+const HEIGHT = 700;
 
-const width =  WIDTH - margin.left - margin.right;
-const height = HEIGHT - margin.top - margin.bottom;
+const widthb =  WIDTH - margin.left - margin.right;
+const heightb = HEIGHT - margin.top - margin.bottom;
 
 const containerBarchart = d3.select('#bar')
     .append('svg')
@@ -15,8 +15,8 @@ const containerBarchart = d3.select('#bar')
 
 
 const scale = d3.scaleLinear()
-                .range([margin.right, width])
-                .domain([0, 70000]);
+                .range([margin.right, widthb])
+                .domain([0, 80000]);
 
 const xAxis = d3.axisBottom(scale).ticks(7);
 const axis = containerBarchart.append('g')
@@ -25,20 +25,25 @@ const axis = containerBarchart.append('g')
 axis.call(xAxis)
 
 containerBarchart.append("text")
-        .attr("x", (width / 2))             
+        .attr("x", (widthb / 2))             
         .attr("y", 0 - (margin.top / 2))
         .attr("text-anchor", "middle")  
-        .style("font-size", "16px") 
-        .style("text-decoration", "underline")  
-        .text("Inmigrantes totales por país");
+        .style("font-size", "16px")
+        .style("fill", "#555")
+        .style("text-decoration", "underline")
+        .text("Stars en módulos de d3.js");
 
 const lista_final = [];
 
-const actualizarBarchart = (cantidad, label) => {
-    lista_final.push({label, cantidad});
-    
+const actualizarBarchart = (nodo, label, nodoclick) => {
+    //clear();
+    //containerBarchart.selectAll('.rect').data(lista_final).remove();
+    //containerBarchart.selectAll('.text').data(lista_final).remove();
+    //lista_final.length = 0;
+    lista_final.push({label, nodo, nodoclick});
+    //console.log(cantidad)
     // Actualizamos el dominio de la escala
-    scale.domain([0, d3.max(lista_final, d=> d.cantidad)])
+    scale.domain([0, d3.max(lista_final, d=> d.nodo.stars)])  // d3.max(lista_final, d=> parseInt(d.stars))
     // Volvemos a aplicar el método call para que el eje se actualice a la 
     // nueva escala
     axis.call(xAxis)
@@ -49,30 +54,103 @@ const actualizarBarchart = (cantidad, label) => {
         .append('rect')
         .attr('class', 'rect')
         .attr('x', margin.right)
-        .attr('y', (_, i) => i*30 + 15)
+        .attr('y', (_, i, nodoclick) => i*30 + 15)
         .attr('height', 20)
+        /*if(nodo == nodoclick){
+            //enteringBar.attr('class', 'rectclick')
+            enteringBar.attr('stroke', 'black')
+            enteringBar.attr('fill', '#FD8D3C')
+            enteringBar.attr('stroke_width', '5px')
+        }else{
+            enteringBar.attr('class', 'rect')
+            //enteringBar.attr('stroke', '#a80e06')
+            //enteringBar.attr('stroke_width', '3px')
+        };*/
+
+        
 
     const enteringtext = data.enter()
         .append('text')
         .attr('class', 'text')
-        .attr('x', margin.right)
-        .attr('y', (_, i) => i*30 + 30)   
+        .attr('text-anchor', 'start')
+        .attr('x', margin.right + 40)
+        .attr('y', (_, i, nodoclick) => i*30 + 30);
+
+
+           
         
     // Agregamos el .merge para identificar las barras que ya estaban dentro
     // del dataset antes de agregar nuevos datos.
     data.merge(enteringBar).transition()
-        .duration(500)
-        .attr('width', d => scale(d.cantidad) - margin.right)
+        .duration(400)
+        .attr('width', d => scale(d.nodo.stars) - margin.right) // d => scale(parseInt(d.stars) ) - margin.right
+        //console.log(d.stars)
+        /*if(nodo == nodoclick){
+            enteringBar.attr('border', 4px)
+        };*/
+        /*if(nodo == nodoclick){
+            enteringBar.attr('class', 'rectclick')
+            enteringBar.attr('stroke', '#a80e06')
+            enteringBar.attr('stroke_width', '3px')
+        };
+        if(nodo != nodoclick){
+            enteringBar.attr('class', 'rect')
+            enteringBar.attr('stroke', '#a80e06')
+            enteringBar.attr('stroke_width', '3px')
+        };*/
+
 
     enteringBar.transition()
-        .duration(500)
-        .attr('width', d => scale(d.cantidad) - margin.right);
-   
+        .duration(400)
+        .attr('width', d => scale(d.nodo.stars) - margin.right)  // d => scale(parseInt(d.stars) ) - margin.right
+        .style('stroke', function(d){
+        if(d.nodo == d.nodoclick){
+          return 'black' // #a80e06
+        } else {
+          return '#FD8D3C'
+        }
+    })
+        .attr('fill', function(d){
+        if(d.nodo == d.nodoclick){
+          return 'black'
+        } else {
+          return '#FD8D3C'
+        }
+
+         
+         // or maybe also this.setAttribute('moved', 'no');
+     })
+        .style('stroke-width', function(d){
+        if(d.nodo == d.nodoclick){
+          return 6
+        } else {
+          return 2
+        }
+
+         
+         // or maybe also this.setAttribute('moved', 'no');
+     })/*if(nodo == nodoclick){
+                enteringBar.attr('class', 'rectclick')
+                //enteringBar.attr('stroke', '#a80e06')
+                //enteringBar.attr('stroke_width', '3px')
+            };
+            if(nodo != nodoclick){
+                enteringBar.attr('class', 'rect')
+                //enteringBar.attr('stroke', '#a80e06')
+                //enteringBar.attr('stroke_width', '3px')
+            };*/
+        
+        
+
     enteringtext.transition()
-        .delay(500)
-        .text(d => d.label);
+        .delay(400)
+        .text(d => d.label); // d => d.name
 
     axis.attr('transform', `translate(0,${lista_final.length*30 + 15})`)
+
+    
+
+
 }
 
 const clear = () => {
---
+++ t02_infovis/graph.js
@@ -0,0 +1,298 @@
+
+var width = 700; //800
+var height = 800; // 600
+//var color = d3.scaleOrdinal(d3.schemeCategory10);
+var color = d3.scaleSequential()
+        .domain([0, 59])
+        .interpolator(d3.interpolateOranges);
+
+
+
+
+
+d3.json("dataset.json").then(function(graph) {
+
+var label = {
+    'nodes': [],
+    'links': []
+};
+
+graph.nodes.forEach(function(d, i) {
+    label.nodes.push({node: d});
+    label.nodes.push({node: d});
+    label.links.push({
+        source: i * 2,
+        target: i * 2 + 1
+    });
+});
+
+var labelLayout = d3.forceSimulation(label.nodes)
+    .force("charge", d3.forceManyBody().strength(-50))
+    .force("link", d3.forceLink(label.links).distance(0).strength(2));
+
+var graphLayout = d3.forceSimulation(graph.nodes)
+    .force("charge", d3.forceManyBody().strength(-2200))
+    .force("center", d3.forceCenter((width / 2) + 80, (height / 2) - 60))
+    .force("x", d3.forceX(width / 2).strength(1))   // d3.forceX(width / 2).strength(1)
+    .force("y", d3.forceY(height / 2).strength(1))  // d3.forceY(height / 2).strength(1)
+    .force("link", d3.forceLink(graph.links).id(function(d) {return d.name; }).distance(50).strength(1))
+    .on("tick", ticked);
+
+var adjlist = [];
+
+
+graph.links.forEach(function(d) {
+    adjlist[d.source.index + "-" + d.target.index] = true;
+    adjlist[d.target.index + "-" + d.source.index] = true;
+});
+
+
+
+function neigh(a, b) {
+    return a == b || adjlist[a + "-" + b];
+}
+
+function colorLink(l){
+  if(l == false){
+    //console.log(l)
+    return "#aaa";
+  }else{
+    return "#305473";
+  }
+}
+
+
+var svg = d3.select("#viz").attr("width", width).attr("height", height);
+var container = svg.append("g");
+
+container.append("container:defs")
+          .append("container:marker")
+          .attr("id", "arrow")
+          .attr("viewBox", "0 -5 10 10")
+          .attr("refX", 20) // 15 mientras menos se acerca al circulo 24
+          .attr("refY", 0) //-1.5
+          .attr("markerWidth", 3) //6
+          .attr("markerHeight", 3) //6
+          .attr("orient", "auto-start-reverse")
+          .append("path")
+          .attr("d", "M0,-5L10,0L0,5") // -5 -> -10 "M0,-5L10,0L0,5" "d", "M 0 -5 10 10" 'M 0,-5 L 10 ,0 L 0,5'
+          //.attr("d", "M 0 -5 10 10")
+          .style("fill", "#9ECAE1");
+
+/*svg.call(
+    d3.zoom()
+        .scaleExtent([.1, 4])
+        .on("zoom", function() { container.attr("transform", d3.event.transform); })
+);*/
+
+var link = container.append("g") //.attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("stroke-width", "4px")
+    .attr("marker-start", "url(#arrow)")
+    .attr("stroke", function(d) { return d.dev == false ? "#9ECAE1" : "#305473"; }); // si es false #9ECAE1, true "#305473"
+
+
+var node = container.append("g")
+    .selectAll("g")
+    .data(graph.nodes)
+    .enter()
+    .append("circle") // en el css fill: #FD8D3C;
+    .attr("r", 10)  // era 10 function(d) { return Math.log(d.forks) * 4; })
+    //.attr("fill", FD8D3C)
+    .attr('class', 'node')
+    .attr('class', 'inactive ball')  // color activo en css #fill: #de7701;
+    .attr("fill", function(d) { return color(d.forks); });
+
+const originx = node.x;
+const originy = node.y;
+
+node.on("mouseover", focus).on("mouseout", unfocus);
+
+
+
+node.call(
+    d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+);
+
+// cuando se hace click en el nodo
+node.on('click', returnPosition);
+
+node.on('dblclick', (d) =>{
+        var cir = d3.selectAll('.inactive').filter(circle => circle == d);
+        var index = d3.select(d3.event.target).datum().index;
+
+        if (cir.size()){
+            cir.attr('class', 'active')
+            .attr("fill", function(d) { return color(d.forks); })
+            actualizarBarchart(d, d.name, d)
+            console.log(index)
+            link.each(function(o, i) {
+              if (o.target.index == index){
+                actualizarBarchart(o.source, o.source.name, d)
+                console.log(o.target.stars, o.target.name)
+                console.log(o.source.stars, o.source.name)
+              }if(o.source.index == index){
+                  actualizarBarchart(o.target, o.target.name, d)
+                  //console.log(o.source.stars, o.source.name)
+              }
+            })
+        }
+      });
+
+var labelNode = container.append("g") //.attr("class", "labelNodes")
+    .selectAll("text")
+    .data(label.nodes)
+    .enter()
+    .append("text")
+    .text(function(d, i) { return i % 2 == 0 ? "" : d.node.name + " # " + d.node.forks; })
+    .style("fill", "#555")
+    .style("font-family", "Arial")
+    .style("font-size", 12)
+    .style("pointer-events", "none"); // to prevent mouseover/drag capture
+
+labelNode.each(function(d, i) {
+        if(i % 2 == 0) {
+            const originx = d.node.x;
+            const originy = d.node.y;
+        }
+        });
+
+node.on("mouseover", focus).on("mouseout", unfocus);
+
+// cuando se hace click en el botón
+d3.select('#clear').on('click', (_) =>{
+        clear();
+        d3.selectAll('circle').attr('class', 'inactive ball');
+        //d3.selectAll('circle').remove();
+    });
+
+function ticked() {
+
+    node.call(updateNode);
+    link.call(updateLink);
+
+    labelLayout.alphaTarget(0.3).restart();
+    labelNode.each(function(d, i) {
+        if(i % 2 == 0) {
+            d.x = d.node.x;
+            d.y = d.node.y;
+        } else {
+            //var b = this.getBBox();
+            //console.log(b);
+            var diffX = d.x - d.node.x;
+            var diffY = d.y - d.node.y;
+
+            var dist = Math.sqrt(diffX * diffX + diffY * diffY);
+
+            var shiftX =  30 * (diffX - dist) / (dist * 2); // b.width
+            shiftX = Math.max(-30, Math.min(0, shiftX)); // b.width
+            var shiftY = 16; // 16
+            this.setAttribute("transform", "translate(" + shiftX + "," + shiftY + ")");
+        }
+    });
+    labelNode.call(updateNode);
+
+}
+
+function fixna(x) {
+    if (isFinite(x)) return x;
+    return 0;
+}
+
+function focus(d) {
+    var index = d3.select(d3.event.target).datum().index;
+    node.style("opacity", function(o) {
+        return neigh(index, o.index) ? 1 : 0.1;
+    });
+    labelNode.attr("display", function(o) {
+      return neigh(index, o.node.index) ? "block": "none";
+    });
+    link.style("opacity", function(o) {
+        return o.source.index == index || o.target.index == index ? 1 : 0.1;
+    });
+}
+
+function unfocus() {
+   labelNode.attr("display", "block");
+   node.style("opacity", 1);
+   link.style("opacity", 1);
+}
+
+function returnPosition(d) {
+    console.log(d.fx);
+    console.log(d.fy);
+    d.fx = originx;
+    d.fy = originy;
+    console.log("hice click");
+    
+    
+}
+
+function updateLink(link) {
+    link.attr("x1", function(d) { return fixna(d.source.x); })
+        .attr("y1", function(d) { return fixna(d.source.y); })
+        .attr("x2", function(d) { return fixna(d.target.x); })
+        .attr("y2", function(d) { return fixna(d.target.y); });
+}
+
+function updateNode(node) {
+    node.attr("transform", function(d) {
+        return "translate(" + fixna(d.x) + "," + fixna(d.y) + ")";
+    });
+}
+
+function dragstarted(d) {
+    //d3.select(this).classed("fixed", d.fixed = true);
+    d3.event.sourceEvent.stopPropagation();
+    if (!d3.event.active) graphLayout.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+  if(d.fx > 750){
+    d.fx = 700;
+  }else if (d.fx < 100) {
+    d.fx = 100;
+  }else{
+    d.fx = d3.event.x;
+  }
+  if(d.fy > 550){
+    d.fy = 500;
+  }else if (d.fy < 40) {
+    d.fy = 40;
+  }else{
+    d.fy = d3.event.y;
+  }
+}
+
+function dragended(d) {
+    if (!d3.event.active) graphLayout.alphaTarget(0);
+    if(d.fx > 750){
+        d.fx = 700;
+      }else if (d.fx < 100) {
+        d.fx = 100;
+      }else{
+        d.fx = d3.event.x;
+      }
+      if(d.fy > 550){
+        d.fy = 500;
+      }else if (d.fy < 40) {
+        d.fy = 40;
+      }else{
+        d.fy = d3.event.y;
+      }
+
+
+    
+      // NULL
+    //console.log(d.y, d.fy);
+}
+
+}); // d3.json
